7e3560c4b3729f4202af8f90368352bbbcb1fb7f
chetan-clicentrix <chetan@clicentrix.com>
2026-01-14 11:27:55 +0530

    fix typescript

--- src/services/chunkUpload.service.ts
+++ src/services/chunkUpload.service.ts
@@ -148,7 +148,7 @@ class ChunkUploadService {
             const hash = crypto.createHash("sha256");
             const stream = fs.createReadStream(filePath);
 
-            stream.on("data", (data) => hash.update(data));
+            stream.on("data", (data: Buffer) => hash.update(data as crypto.BinaryLike));
             stream.on("end", () => resolve(hash.digest("hex")));
             stream.on("error", reject);
         });
