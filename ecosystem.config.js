0dce07c7f805ef4918954f0956afafb4341ffa1e
chetan-clicentrix <chetan@clicentrix.com>
2026-02-10 11:15:59 +0530

    code changed

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,49 +1,8 @@
 module.exports = {
   apps: [
-    /* =========================
-       FRONTEND (React / Vite)
-       ========================= */
+
     {
-      name: "cxonego-frontend",
-      script: "server.cjs",
-
-      instances: 1,
-      exec_mode: "fork",
-      watch: false,
-      autorestart: true,
-
-      // Memory tuning
-      // Heap: 2GB | Restart if real leak
-      node_args: "--max-old-space-size=2048",
-      max_memory_restart: "2200M",
-
-      // Environment
-      env: {
-        NODE_ENV: "production",
-        PORT: 5173
-      },
-
-      // Logs
-      error_file: "logs/frontend-error.log",
-      out_file: "logs/frontend-out.log",
-      merge_logs: true,
-      log_date_format: "YYYY-MM-DD HH:mm:ss",
-
-      // Stability
-      max_restarts: 10,
-      restart_delay: 4000,
-      kill_timeout: 3000,
-      wait_ready: true,
-      listen_timeout: 10000,
-
-      source_map_support: false
-    },
-
-    /* =========================
-       BACKEND (Node API)
-       ========================= */
-    {
-      name: "cxonego-backend-main",
+      name: "cxonego-backend",
       script: "build/src/index.js",
 
       // 2 vCPU → 2 instances
